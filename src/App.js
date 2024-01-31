5c539d9a4d0d4615069ef6cdc9a03b31348d215e
infomani23 <dbamanikandan23@gmail.com>
2024-01-31 17:29:35 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.error("Error fetching data: ", err));
+  }, []);
+  const cardStyle = {
+    width:"200px",
+    border:"1px solid #ccc",
+    borderRadius:"10px",
+    margin:"10px",
+    padding:"10px",
+    display:"flex",
+    flexDirection:"column",
+    alignItems:"center",
+    justifyContent:"center",
+  };
+const imageStyle = {
+  width:"100px",
+  height:"100px",
+
+};
+const containerStyle = {
+  display:"flex",
+  flexWrap:"wrap",
+  justifyContent:"center",
+  alignItems:"center",
+  height:"100vh",
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      
+      {countries.map((country, index) => (
+        <div key={country.cca3} className="country-card" style={cardStyle}>
+          <h2>{country.name.common}</h2>
+          {country.flags && country.flags.png ? (
+            <img
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`} style={imageStyle}
+            />
+          ) : (
+            <p>No flag available</p>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
